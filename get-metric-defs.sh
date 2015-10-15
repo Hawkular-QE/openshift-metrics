2cb77ec371ffe2dc3ef17f8f0009987f722a7f7c
Matt Mahoney <mmahoney@redhat.com>
2015-10-15 17:28:40 -0400

    MS5 Build-2 Refactoring

--- get-metric-defs.sh
+++ get-metric-defs.sh
@@ -3,7 +3,10 @@
 
 . ./env
 OUTPUT=output/metrics-defs.json
-curl -s "${HEADERS[@]}" ${METRICS_ENDPOINT}/metrics | python -m json.tool > $OUTPUT
+TOKEN=$(oc whoami -t)
+TENANT="metrics"
+
+curl -H "Authorization: Bearer ${TOKEN}" -H "hawkular-tenant: ${TENANT}" "${HEADERS[@]}" -X GET ${METRICS_ENDPOINT}/metrics | python -m json.tool > $OUTPUT
 echo "Saved to $OUTPUT"
 echo "..."
 tail -10 $OUTPUT
